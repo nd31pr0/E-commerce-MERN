e5d1ec8590be6e32c4945f3c433f5124cab537e6
Ndzi Nfor <ndzinfor@gmail.com>
2025-04-17 14:41:53 +0100

    connect to DB

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import {connectDB} from './config/db.js';
+
+dotenv.config(); // helps our mongo_uri to be available in process.env.MONGO_URI
 
 const app = express();
 
@@ -8,5 +12,6 @@ app.get("/products", (req, res) => {
 
 
 app.listen(5000, () => {
-  console.log('Server started at http://localhost:5000');
+    connectDB();
+    console.log('Server started at http://localhost:5000');
 });
