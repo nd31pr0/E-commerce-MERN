dcecbbb51e04da04bf444b55652780b930d066c7
Ndzi Nfor <ndzinfor@gmail.com>
2025-04-18 09:36:48 +0100

    create api to display all products

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,15 @@ const app = express();
 
 app.use(express.json()); // to parse incoming JSON requests
 
+app.get('/api/products', async(req, res) => {
+    try{
+        const products = await Product.find();
+        res.status(200).json({success: true, data: products})
+    } catch (error) {
+        res.status(500).json({success: false, message: 'No products exist'})
+    }
+})
+
 app.post('/api/products', async (req, res) => {
     const product = req.body;
 
@@ -27,9 +36,17 @@ app.post('/api/products', async (req, res) => {
     }
 })
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the home page');
+app.delete('/api/products/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "Product deleted"});
+    } catch (error) {
+        res.status(404).json({success:false, message: 'Product not found'});
+    }
 })
+
+
 
 app.listen(5000, () => {
     connectDB();
