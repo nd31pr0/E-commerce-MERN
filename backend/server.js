c480ceae0ac10a1e3713f39c0fbe62f602dae241
Ndzi Nfor <ndzinfor@gmail.com>
2025-04-18 22:14:11 +0100

    refactor backend code

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,12 @@ import productRoutes from './routes/product.route.js';
 dotenv.config(); // helps our mongo_uri to be available in process.env.MONGO_URI
 
 const app = express();
+const PORT = process.env.PORT || 5000; // if process.env.PORT is not defined, use 5000
 
 app.use(express.json()); // to parse incoming JSON requests
 
 app.use("/api/products", productRoutes); // to use the product routes)
 app.listen(5000, () => {
     connectDB();
-    console.log('Server started at http://localhost:5000');
+    console.log('Server started at http://localhost:' + PORT);
 });
