59cd929367065e607e677705c7ab865fcfc49e2f
Ndzi Nfor <ndzinfor@gmail.com>
2025-04-19 07:37:47 +0100

    create frontend routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </StrictMode>
 )
