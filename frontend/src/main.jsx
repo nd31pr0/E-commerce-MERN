582daeee560931e41c28888d0f3c6c33db349d5c
Ndzi Nfor <ndzinfor@gmail.com>
2025-04-20 07:10:52 +0100

    adjust colors for lightmode and darkmode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import { Provider } from "@/components/ui/provider"
 import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './App.jsx'
