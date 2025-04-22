bdbb22b964fbe8553fedd5e861e42effa63fd505
Ndzi Nfor <ndzinfor@gmail.com>
2025-04-22 09:39:43 +0100

    log products on homePage to console

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,15 @@
-import { VStack, Container, Text } from '@chakra-ui/react'
+import { VStack, Container, Text, SimpleGrid } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useProductStore } from '../store/product'
 const HomePage = () => {
+  const {fetchProducts, products} = useProductStore()
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts])  
+  console.log("products", products)
+
   return (
     <Container maxW='container.xl' py={12}>
         <VStack spacing={8}>
@@ -13,6 +22,17 @@ const HomePage = () => {
         >
             Current Products  
         </Text>
+
+        <SimpleGrid
+            columns={{ 
+                base: 1, 
+                md: 2, 
+                lg: 3 
+            }}
+            spacing={10}
+            w={"full"}
+        >
+        </SimpleGrid>
 
         <Text
             fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
