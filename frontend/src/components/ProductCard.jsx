f7ff2baf68d75f543d57b136f1006aade4961f4b
Ndzi Nfor <ndzinfor@gmail.com>
2025-04-25 00:39:13 +0100

    add onClick event or delete button

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,8 +1,37 @@
-import {Box, HStack, Heading, IconButton, Image, Text, useColorModeValue} from "@chakra-ui/react"
+import {Box, HStack, Heading, IconButton, Image, Text, useColorModeValue, useToast} from "@chakra-ui/react"
 import { EditIcon, DeleteIcon } from "@chakra-ui/icons"
+import { useProductStore } from "../store/product"
 const ProductCard = ({product}) => {
   const textColor = useColorModeValue("gray.600", "gray.200")
   const bg = useColorModeValue("white", "gray.800")
+
+  const {deleteProduct} = useProductStore()
+  const toast = useToast()
+
+  const handleDeleteProduct = async (id) => {
+    const {success, message} = await deleteProduct(id)
+    if(!success){
+        toast(
+            {
+                title: "Error",
+                description: message,
+                status: "error",
+                duration: 3000,
+                isClosable: true,
+            })
+    } else {
+        toast(
+            {
+                title: "Success",
+                description: message,
+                status: "success",
+                duration: 3000,
+                isClosable: true,
+            }
+        )
+    }
+ 
+  }
   return (
     <Box
     shadow='lg'
@@ -25,7 +54,7 @@ const ProductCard = ({product}) => {
 
             <HStack spacing={2}>
                 <IconButton icon={<EditIcon />} colorScheme='blue' />
-                <IconButton icon={<DeleteIcon />} colorScheme='red' />
+                <IconButton icon={<DeleteIcon />} onClick={handleDeleteProduct} colorScheme='red' />
             </HStack>
         </Box>
     </Box>
