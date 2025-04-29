4ead1866367d1e596e2c365df8cf19eca00ae2d7
Ndzi Nfor <ndzinfor@gmail.com>
2025-04-29 22:49:02 +0100

    serve the frontend files in production

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,24 @@ import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from './config/db.js';
 import productRoutes from './routes/product.route.js';
+import path from 'path';
 
 dotenv.config(); // helps our mongo_uri to be available in process.env.MONGO_URI
 
 const app = express();
 const PORT = process.env.PORT || 5000; // if process.env.PORT is not defined, use 5000
 
+const __dirname = path.resolve(); // helps to get the current directory
 app.use(express.json()); // to parse incoming JSON requests
 
 app.use("/api/products", productRoutes); // to use the product routes)
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
 app.listen(5000, () => {
     connectDB();
     console.log('Server started at http://localhost:' + PORT);
